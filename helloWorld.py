425691c6ac619e19fc3a0ccfd4960dff19499c77
mecazawab <vice0703@gmail.com>
2018-09-22 02:24:09 +0900

    演習　横並び

--- helloWorld.py
+++ helloWorld.py
@@ -1,2 +1,22 @@
-print("Hello byby")
-print("Hello World!")
+num = 1
+while num <= 30:
+    if num % 10 == 0:
+        print(num)
+    else:
+        if num < 10:
+            print(" " + str(num), end=" ")
+        else:
+            print(num, end=" ")
+    num += 1
+'''
+num = 1
+while num <= 30:
+    numForm = num
+    if num < 10:
+        numForm = " " + str(num)
+    if num % 10 == 0:
+        print(numForm)
+    else:
+        print(numForm, end=" ")
+    num += 1
+'''
