41e1dcb4ca592c3b5ce2cb1a5aa61a3fe100f5ff
KC <kcsp1990@gmail.com>
2018-09-23 00:27:20 +0900

    コメントの追加

--- helloWorld.py
+++ helloWorld.py
@@ -1,10 +1,16 @@
+'''
+9以下の場合空白を足す関数
+'''
 def is_one_digit(num):
     result = num
     if num < 10:
         result = " " + str(num)
     return result
 
+# 初期化
 num = 1
+
+# 1から30までコンソールに出力する処理
 while num <= 30:
     numForm = num
     numForm = is_one_digit(numForm)
