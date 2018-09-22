39e29eb7a72ef31927893bca1a39b7e46be0a381
KC <kcsp1990@gmail.com>
2018-09-23 00:47:07 +0900

    表示する数字のMAX値を入力するように変更 桁合わせを3桁に変更

--- helloWorld.py
+++ helloWorld.py
@@ -4,14 +4,19 @@
 def is_one_digit(num):
     result = num
     if num < 10:
+        result = "  " + str(num)
+    elif num < 100:
         result = " " + str(num)
     return result
 
+
 # 初期化
+x = input("Please Enter Number: ")
+maxNum = int(x)
 num = 1
 
 # 1から30までコンソールに出力する処理
-while num <= 30:
+while num <= maxNum:
     numForm = num
     numForm = is_one_digit(numForm)
     if num % 10 == 0:
