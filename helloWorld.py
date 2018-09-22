d52f9dd9f9fba974cbeb65d981dc7381ca4a3b47
KC <kcsp1990@gmail.com>
2018-09-23 00:22:22 +0900

    9以下の整形を関数に定義

--- helloWorld.py
+++ helloWorld.py
@@ -1,22 +1,15 @@
-num = 1
-while num <= 30:
-    if num % 10 == 0:
-        print(num)
-    else:
-        if num < 10:
-            print(" " + str(num), end=" ")
-        else:
-            print(num, end=" ")
-    num += 1
-'''
+def is_one_digit(num):
+    result = num
+    if num < 10:
+        result = " " + str(num)
+    return result
+
 num = 1
 while num <= 30:
     numForm = num
-    if num < 10:
-        numForm = " " + str(num)
+    numForm = is_one_digit(numForm)
     if num % 10 == 0:
         print(numForm)
     else:
         print(numForm, end=" ")
     num += 1
-'''
