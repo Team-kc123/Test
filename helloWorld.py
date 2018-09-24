7cca42dc7bea489f2b6b168f336966a38a4f21be
KC <kcsp1990@gmail.com>
2018-09-25 01:17:13 +0900

    Unityで作った神経衰弱

--- helloWorld.py
+++ helloWorld.py
@@ -1,13 +1,11 @@
-'''
-9以下の場合空白を足す関数
-'''
-def is_one_digit(num):
-    result = num
-    if num < 10:
-        result = "  " + str(num)
-    elif num < 100:
-        result = " " + str(num)
-    return result
+# 9以下の場合空白を足す関数
+def is_one_digit(_num):
+	result = _num
+	if _num < 10:
+		result = "  " + str(_num)
+	elif _num < 100:
+		result = " " + str(_num)
+	return result
 
 
 # 初期化
@@ -17,10 +15,10 @@ num = 1
 
 # 1から30までコンソールに出力する処理
 while num <= maxNum:
-    numForm = num
-    numForm = is_one_digit(numForm)
-    if num % 10 == 0:
-        print(numForm)
-    else:
-        print(numForm, end=" ")
-    num += 1
+	numForm = num
+	numForm = is_one_digit(numForm)
+	if num % 10 == 0:
+		print(numForm)
+	else:
+		print(numForm, end=" ")
+	num += 1
